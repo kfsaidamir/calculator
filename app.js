6d3fd808c07934ff724bf209adb314e1610ea5fe
kf_saidamir <skadirov2009@gmail.com>
2024-03-09 15:18:35 +0500

    calculator

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 function a() {
-    var a,b,c,d,f,
+    var a,b,c,f,
     a = prompt('Выберите + - * /')
     b = +prompt('первая цифра')
-    c = +prompt('вторая цифра')
+    c = +prompt('вторая цифра') 
+    d = +prompt('третья цифра')
+    e = +prompt('четвертая цифра')
     if(a=='+'){
-    f=Number(b)+Number(c)
+        f=Number(b+c+d+e)
     }else if(a=='-'){
-        f=Number(b)-Number(c)
+        f=Number(b-c-d-e)
     }else if(a=='*'){
-        f=Number(b)*Number(c)
+        f=Number(b*c*d*e)
     }else if(a=='/') {
-        f=Number(b)/Number(c)
-    }
-    alert(`${b} ${a} ${c} = ${f}`)
+        f=Number(b/c/d/e)
+     } alert(`${b} ${a} ${c} ${a} ${d} ${a} ${e} = ${f}`)
 }a()
 
